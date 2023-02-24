ae7e5fd77263d768c2d6841bc9a7ce4c1b23ea5d
Roman <roman.comm1@gmail.com>
2023-02-24 12:58:56 +0300

    deploy fix

--- server.js
+++ server.js
@@ -13,8 +13,8 @@
 //   res.sendFile(path.join(__dirname+'/dist/annotation/index.html'));
 // });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
 
 
 const express = require('express');
