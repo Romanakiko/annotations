f1331bcec07912939c83cf22d29bbf744b9de4e5
Roman <roman.comm1@gmail.com>
2023-02-24 03:31:07 +0300

    fix build properties

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 
 //Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/annotation'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/annotation/index.html'));
-});
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+//
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/annotation'));
+//
+// app.get('/*', function(req,res) {
+//
+//   res.sendFile(path.join(__dirname+'/dist/annotation/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('./dist/'));
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/' });
+});
+
 app.listen(process.env.PORT || 8080);
